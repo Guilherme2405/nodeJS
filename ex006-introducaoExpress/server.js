e3f47452678d50233d046b45e4754fcbfccc055a
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-06-14 00:39:39 -0300

    subindo servido node.js

--- ex006-introducaoExpress/server.js
+++ ex006-introducaoExpress/server.js
@@ -4,7 +4,7 @@ const app = express();
 //CRUD -> CREATE    READ    UPDATE   DELETE
 //JS   ->  POST      GET     PUT      DELETE
 
-app.get('/', (req,resp) =>{
+app.get('/', (req,resp) =>{                                   
     resp.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(3030, () => console.log('servidor rodando na porta http://localhost:3030'));
