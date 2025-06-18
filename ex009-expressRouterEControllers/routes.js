780bb08d3e5fab70652cbe4c6f1e13681719e3d6
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2025-06-17 22:24:58 -0300

    att

--- ex009-expressRouterEControllers/routes.js
+++ ex009-expressRouterEControllers/routes.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const route = express.Router();
+const express = require('express'); // utiliza o express
+const route = express.Router(); // utiliza funçoes do router(rotas) do express
 const homeController = require("./controllers/homecontroller");
-const contatoController = require("./controllers/contatoController")
+const contatoController = require("./controllers/contatoController");
 
 // rotas home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/', homeController.paginaInicial); // importa a funcao paginaInicial do arquivo(homeController) para dar acesso e mostrar no servidor '/'
+route.post('/', homeController.trataPost);// importa a funcao trataPost do arquivo(homeController) para dar acesso e mostrar no servidor '/'
 
 // rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+route.get('/contato', contatoController.paginaInicial); // importa a funcao paginaInicial do arquivo(homeController) para dar acesso e mostrar no servidor '/contato'
 
 module.exports = route;
--- ex009-expressRouterEControllers/server.js
+++ ex009-expressRouterEControllers/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const routes = require("./routes");
 
-app.use(express.urlencoded({extended:true}));
-app.use(routes);
+app.use(express.urlencoded({extended:true})); // permite tratar os dados recebidos
+app.use(routes); // permite usa os dados do arquivo routes
 
 
-app.listen(3000, () => console.log('servidor rodando na porta http://localhost:3000'));
+app.listen(3000, () => console.log('servidor rodando na porta http://localhost:3000')); // executa o servidor na porta 3000
